a418ebe20d3bb65ce1677a7fbf03d2c1324eeed8
renross <briross@udel.edu>
2026-03-01 20:30:41 -0500

    file renames

--- src/roman_converter/ross_b.py
+++ src/roman_converter/ross_b.py
@@ -11,9 +11,12 @@ def to_roman(arabic: int) -> str:
     return conversion
 
 def from_roman(roman: str) -> int:
-    arabic_map: list[tuple[str, int]] = [ # pair letters to numerals
+    arabic_map:list[tuple[str,int]] = [ # pair letters to numerals
         ("I", 1), ("IV", 4), ("V", 5), ("IX", 9), ("X", 10),
         ("XL", 40), ("L", 50), ("XC", 90), ("C", 100),
         ("CD", 400), ("D", 500), ("CM", 900), ("M", 1000)
     ]
+    conversion: int = 0
+
+
     return 0
